b881e7e1f002f26280df912278a07c060a5b28d9
John Nolan <90651514+Fediry@users.noreply.github.com>
2025-09-11 23:06:06 -0700

    copied in template config.h and rules.mk

---
+++ config.h
@@ -0,0 +1,16 @@
+// Copyright 2022 Pavel Glushkov (@pashutk)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define MATRIX_ROWS  8
+#define MATRIX_COLS  6
+
+#define COMBO_ONLY_FROM_LAYER 0
+
+#define ONESHOT_TAP_TOGGLE 2
+
+#define USE_SERIAL
+#define SOFT_SERIAL_PIN D2
+
+#define MK_KINETIC_SPEED
